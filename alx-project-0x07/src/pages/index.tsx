a0b4bda6519e43d606c9cc7ab1e810577119174b
Bethel Amadi <amadibethel@icloud.com>
2025-10-06 20:06:18 +0200

    Add updated file to Project

--- alx-project-0x07/src/pages/index.tsx
+++ alx-project-0x07/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-
 const Home: React.FC = () => {
-  const handleGenerateImage = async () => {
-    console.log("Generating Images")
+  const [prompt, setPrompt] = useState<string>("");
+
+  const handleGenerateImage = () => {
+    console.log("Generating Images for prompt:", prompt);
   };
 
   return (
@@ -17,6 +18,8 @@ const Home: React.FC = () => {
         <div className="w-full max-w-md">
           <input
             type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
             className="w-full p-3 border border-gray-300 rounded-lg mb-4"
           />
@@ -28,7 +31,7 @@ const Home: React.FC = () => {
           </button>
         </div>
       </div>
-     </div>
+    </div>
   );
 };
 
